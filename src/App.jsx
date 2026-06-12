08516e111e24f4105d28bcd3c999f8e637006a35
Pragnasriyerra <237r1a66c7@cmrtc.ac.in>
2026-06-12 11:53:16 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 import VenueDetails from "./pages/VenueDetails";
+import MasterSignIn from "./pages/MasterSignIn";
+import OwnerSignIn from "./pages/OwnerSignIn";
+import OwnerSignUp from "./pages/OwnerSignUp";
+import PlayerSignIn from "./pages/PlayerSignIn";
+import PlayerSignUp from "./pages/PlayerSignUp";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route
-          path="/venue/:id"
-          element={<VenueDetails />}
-        />
+        <Route path="/venue/:id" element={<VenueDetails />} />
+
+        <Route path="/master-signin" element={<MasterSignIn />} />
+        <Route path="/owner-signin" element={<OwnerSignIn />} />
+        <Route path="/owner-signup" element={<OwnerSignUp />} />
+        <Route path="/player-signin" element={<PlayerSignIn />} />
+        <Route path="/player-signup" element={<PlayerSignUp />} />
       </Routes>
     </BrowserRouter>
   );
